6e3fb3af0f7823e66061842541f9ece6bf429014
MVS Sriharsha <sriharshamvs@gmail.com>
2020-02-01 10:57:40 +0530

    Autormated '.env' file generation

--- scrapeSingleGroup.py
+++ scrapeSingleGroup.py
@@ -1,64 +1,99 @@
 from telethon import TelegramClient, sync
+from dotenv import load_dotenv
+import os
 import csv
 
-api_id = '1128991'
-api_hash = '6f7c255b1da43aed877093d6d20463b3'
-phone = '+919866655322'
+def main():
+    # Get ENV variables from the '.env' file
+    load_dotenv()
+    api_id = os.getenv('API_ID')
+    api_hash = os.getenv('API_HASH')
+    phone = os.getenv('PHONE')
+    fileName = os.getenv('FILENAME')
 
-client = TelegramClient(phone, api_id, api_hash).start()
+    client = TelegramClient(phone, api_id, api_hash)
 
-groups_list = []
-group = {}
+    client.connect()
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        client.sign_in(phone, input('Enter the OTP: '))
 
-for d in client.get_dialogs():
-    try:
-        if d.is_group and not d.is_channel and d.name != '':
-            group = {
-                "id": d.entity.id,
-                "title" : d.entity.title
-            }
-            groups_list.append(group)
-    except:
-        continue
+    groups_list = []
+    group = {}
 
-print('Choose a group to scrape members from:')
-i=0
-for g in groups_list:
-    print(str(i) + '- ' + g['title'])
-    i+=1
+    # Get's a List of Dictionaries with Group name with Group ID
+    for d in client.get_dialogs():
+        try:
+            if d.is_group and not d.is_channel and d.name != '':
+                group = {
+                    "id": d.entity.id,
+                    "title" : d.entity.title
+                }
+                groups_list.append(group)
+        except:
+            continue
 
-g_index = input("Enter a Number: ")
-target_group = groups_list[int(g_index)]
+    print('Choose a group to scrape members from:')
+    i=0
+    for g in groups_list:
+        print(str(i) + '- ' + g['title'])
+        i+=1
 
-print('Fetching Members...')
-all_participants = []
+    g_index = input("Enter a Number: ")
+    target_group = groups_list[int(g_index)]
 
-all_participants = client.get_participants(target_group["title"])
+    print('Fetching Members...')
+    all_participants = []
 
-print('Saving In file...')
-with open("members.csv","w",encoding='UTF-8') as f:
-    writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username', 'phone','group', 'user_id', 'group_id'])
-    for user in all_participants:
-        if user.username:
-            username = user.username
-        else:
-            username = ""
-        if user.phone:
-            phone = user.phone
-        else:
-            phone = ""
-        if user.id:
-            user_id = user.id
-        else:
-            user_id = ""
-        if target_group["title"]:
-            group = target_group["title"]
-        else:
-            group = ""
-        if target_group["id"]:
-            group_id = target_group["id"]
-        else:
-            group_id = ""
-        writer.writerow([username, phone, group, user_id, group_id])      
-print('Members scraped successfully.')
+    all_participants = client.get_participants(target_group["title"])
+
+    print('Saving In file...')
+    with open(fileName, mode="w",encoding='UTF-8') as f:
+        writer = csv.writer(f,delimiter=",",lineterminator="\n")
+        writer.writerow(['username', 'phone','group', 'user_id', 'group_id'])
+        for user in all_participants:
+            if user.username:
+                username = user.username
+            else:
+                username = ""
+            if user.phone:
+                phone = user.phone
+            else:
+                phone = ""
+            if user.id:
+                user_id = user.id
+            else:
+                user_id = ""
+            if target_group["title"]:
+                group = target_group["title"]
+            else:
+                group = ""
+            if target_group["id"]:
+                group_id = target_group["id"]
+            else:
+                group_id = ""
+            writer.writerow([username, phone, group, user_id, group_id])      
+    print('Members scraped successfully.')
+
+# Generates '.env' File in the project Directory
+def generateENV():
+    print("Generating ENV File")
+    envData = []
+    dataName = ['API_ID', 'API_HASH', 'PHONE', 'FILENAME']
+    print("Enter the Required Credentials:")
+    index = 0
+    for i in range(4):
+        data = input('{}: '.format(dataName[i]))
+        envData.append(data)
+    with open(".env", mode="w",encoding='UTF-8') as f:
+        for index in range(len(envData)):
+            f.write('{0} = \'{1}\'\n'.format(dataName[index], envData[index]))
+    return
+
+if __name__ == "__main__":
+    print("Checking .env file")
+    if os.path.isfile('.env'):
+        main()
+    else:
+        generateENV()
+        main()
--- scrapeUserGroups.py
+++ scrapeUserGroups.py
@@ -1,76 +1,108 @@
 from telethon import TelegramClient, sync
+from dotenv import load_dotenv
+import os
 import csv
 
-# Enter your Credentials
-api_id = '1128991'
-api_hash = '6f7c255b1da43aed877093d6d20463b3'
-phone = '+919866655322'
-fileName = "users.csv"
+def main():
+    # Get ENV variables from the '.env' file
+    load_dotenv()
+    api_id = os.getenv('API_ID')
+    api_hash = os.getenv('API_HASH')
+    phone = os.getenv('PHONE')
+    fileName = os.getenv('FILENAME')
 
-client = TelegramClient(phone, api_id, api_hash)
+    client = TelegramClient(phone, api_id, api_hash)
 
-client.connect()
-if not client.is_user_authorized():
-    client.send_code_request(phone)
-    client.sign_in(phone, input('Enter the OTP: '))
+    client.connect()
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        client.sign_in(phone, input('Enter the OTP: '))
 
-groups_list = []
-group = {}
+    groups_list = []
+    group = {}
 
-for d in client.get_dialogs():
-    try:
-        if d.is_group and not d.is_channel and d.name != '':
-            group = {
-                "id": d.entity.id,
-                "title" : d.entity.title
-            }
-            groups_list.append(group)
-    except:
-        continue
+    # Get's a List of Dictionaries with Group name with Group ID
+    for d in client.get_dialogs():
+        try:
+            if d.is_group and not d.is_channel and d.name != '':
+                group = {
+                    "id": d.entity.id,
+                    "title" : d.entity.title
+                }
+                groups_list.append(group)
+        except:
+            continue
 
-with open(fileName,"w",encoding='UTF-8') as f:
-        writer = csv.writer(f,delimiter=",",lineterminator="\n")
-        writer.writerow(['username', 'first_name', 'last_name', 'phone','group', 'user_id', 'group_id'])
+    # Create a CSV file
+    with open(fileName, mode="w",encoding='UTF-8') as f:
+            writer = csv.writer(f,delimiter=",",lineterminator="\n")
+            writer.writerow(['username', 'first_name', 'last_name', 'phone','group', 'user_id', 'group_id'])
 
+    # Extract Users from the Group
+    for group_name in groups_list:
+        # Iterates over List of Dictionaries to extract Users
+        # Output CSV file has:- username, first_name, last_name, phone, group, user_id, group_id
 
-for group_name in groups_list:
-    target_group = group_name
-    all_participants = []
-    all_participants = client.get_participants(target_group["title"])
+        target_group = group_name
+        all_participants = []
+        all_participants = client.get_participants(target_group["title"])
 
-    print('Fetching Members from {}\nWriting to the file'.format(target_group["title"]))
+        print('Fetching Members from {}\nWriting to the file'.format(target_group["title"]))
 
-    with open(fileName, mode="a", encoding='UTF-8') as f:
-        writer = csv.writer(f, delimiter=",", lineterminator="\n")
-        for user in all_participants:
-            if user.username:
-                username = user.username
-            else:
-                username = ""
-            if user.first_name:
-                first_name = user.first_name
-            else:
-                first_name = ""
-            if user.last_name:
-                last_name = user.last_name
-            else:
-                last_name = ""
-            if user.phone:
-                phone = user.phone
-            else:
-                phone = ""
-            if user.id:
-                user_id = user.id
-            else:
-                user_id = ""
-            if target_group["title"]:
-                group = target_group["title"]
-            else:
-                group = ""
-            if target_group["id"]:
-                group_id = target_group["id"]
-            else:
-                group_id = ""
-            writer.writerow([username, first_name, last_name, phone, group, user_id, group_id])      
+        with open(fileName, mode="a", encoding='UTF-8') as f:
+            writer = csv.writer(f, delimiter=",", lineterminator="\n")
+            for user in all_participants:
+                if user.username:
+                    username = user.username
+                else:
+                    username = ""
+                if user.first_name:
+                    first_name = user.first_name
+                else:
+                    first_name = ""
+                if user.last_name:
+                    last_name = user.last_name
+                else:
+                    last_name = ""
+                if user.phone:
+                    phone = user.phone
+                else:
+                    phone = ""
+                if user.id:
+                    user_id = user.id
+                else:
+                    user_id = ""
+                if target_group["title"]:
+                    group = target_group["title"]
+                else:
+                    group = ""
+                if target_group["id"]:
+                    group_id = target_group["id"]
+                else:
+                    group_id = ""
+                writer.writerow([username, first_name, last_name, phone, group, user_id, group_id])      
 
-print('All Group Members are scraped successfully.')
+    print('All Group Members are scraped successfully.')
+    
+# Generates '.env' File in the project Directory
+def generateENV():
+    print("Generating ENV File")
+    envData = []
+    dataName = ['API_ID', 'API_HASH', 'PHONE', 'FILENAME']
+    print("Enter the Required Credentials:")
+    index = 0
+    for i in range(4):
+        data = input('{}: '.format(dataName[i]))
+        envData.append(data)
+    with open(".env", mode="w",encoding='UTF-8') as f:
+        for index in range(len(envData)):
+            f.write('{0} = \'{1}\'\n'.format(dataName[index], envData[index]))
+    return
+
+if __name__ == "__main__":
+    print("Checking .env file")
+    if os.path.isfile('.env'):
+        main()
+    else:
+        generateENV()
+        main()
