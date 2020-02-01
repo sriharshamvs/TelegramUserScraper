78d0afca8309087279e06c98aa0562cac5993111
MVS Sriharsha <sriharshamvs@gmail.com>
2020-02-01 11:03:32 +0530

    Automated '.env' file Generation

--- scrapeSingleGroup.py
+++ scrapeSingleGroup.py
@@ -97,3 +97,4 @@ if __name__ == "__main__":
     else:
         generateENV()
         main()
+        
--- scrapeUserGroups.py
+++ scrapeUserGroups.py
@@ -106,3 +106,4 @@ if __name__ == "__main__":
     else:
         generateENV()
         main()
+        
